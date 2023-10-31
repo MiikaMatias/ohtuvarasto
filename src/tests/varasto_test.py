8af6e200aa23ad8a11bc30aed73ae638f3815b78
Miika Piiparinen <miikapiiparinen24@gmail.com>
2023-10-31 15:59:47 +0200

    Kun luokan Varasto (tiedoston src/varasto.py) testien haarautumakattavuus (branch coverage) on 100%, pushaa tekemäsi muutokset GitHubiin

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -12,11 +12,27 @@ class TestVarasto(unittest.TestCase):
 
     def test_uudella_varastolla_oikea_tilavuus(self):
         self.assertAlmostEqual(self.varasto.tilavuus, 10)
+    
+    def test_uusi_varasto_negatiivinen_tilavuus(self):
+        varasto = Varasto(-10)
+        self.assertEqual(varasto.tilavuus, 0)
 
+    def test_uusi_varasto_negatiivinen_saldo(self):
+        varasto = Varasto(10, alku_saldo=-10)
+        self.assertAlmostEqual(varasto.saldo, 0)
+
+    def test_lisays_lisaa_saldoa_yli(self):
+        self.varasto.lisaa_varastoon(18)
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+    
     def test_lisays_lisaa_saldoa(self):
-        self.varasto.lisaa_varastoon(8)
+        self.varasto.lisaa_varastoon(10)
 
-        self.assertAlmostEqual(self.varasto.saldo, 8)
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_lisays_lisaa_saldoa_negatiivinen(self):
+        self.varasto.lisaa_varastoon(-2)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
 
     def test_lisays_lisaa_pienentaa_vapaata_tilaa(self):
         self.varasto.lisaa_varastoon(8)
@@ -38,3 +54,14 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+    
+    def test_väärä_otto_palauttaa_0(self):
+        self.assertAlmostEqual(self.varasto.ota_varastosta(-3), 0)
+
+    def test_kaikki_mita_voidaan(self):
+        self.varasto.lisaa_varastoon(2)
+        kmv = self.varasto.ota_varastosta(3)
+        self.assertAlmostEqual(kmv, 2)
+
+    def test___str__(self):
+        self.assertAlmostEqual("saldo = 0, vielä tilaa 10", self.varasto.__str__())
